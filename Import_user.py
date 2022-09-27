5cfb472a9439957d558b4602095b0af9f1fa3c6d
Megaladon <stanko.demon@gmail.com>
2022-09-27 17:49:44 +0300

    Semenar_7

---
+++ Import_user.py
@@ -0,0 +1,9 @@
+def get_string(data):
+    return data.split()
+
+def look(date, id, search):
+    cort = []
+    for i in range(len(date)):  
+        if search in date[i][id]:
+            cort.append(date[i])
+    return cort
